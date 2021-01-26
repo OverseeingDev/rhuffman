4a593b0e7cf14881d140f4b041bcff19c7f91e2d
Loic LeBlanc <loicleblancprofessionnel@gmail.com>
2021-01-26 00:04:44 -0500

    Basic structopt config for rhuff-compress

--- rhuff-compress/src/main.rs
+++ rhuff-compress/src/main.rs
@@ -1,3 +1,26 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(about = "Data shall be brought to entropy", author)]
+struct Opt {
+    /// Compress input file into output file
+    #[structopt(short = "c", long = "compress", conflicts_with("decompress"))]
+    compress: bool,
+    /// Decompress input file into output file
+    #[structopt(short = "d", long = "decompress", conflicts_with("compress"))]
+    decompress: bool,
+
+    /// Input file
+    #[structopt(parse(from_os_str))]
+    input: PathBuf,
+
+    /// Output file, stdout if not present
+    #[structopt(parse(from_os_str))]
+    output: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    println!("{:?}", opt);
 }
